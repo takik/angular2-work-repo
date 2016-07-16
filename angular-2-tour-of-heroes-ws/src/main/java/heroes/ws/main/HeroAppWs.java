83fba9a49ff33d9294f57dd0a7a5c0cf16a38a9f
takik <tarak.akik@gmail.com>
2016-07-16 21:38:50 +0200

    YHehah

--- angular-2-tour-of-heroes-ws/src/main/java/heroes/ws/main/HeroAppWs.java
+++ angular-2-tour-of-heroes-ws/src/main/java/heroes/ws/main/HeroAppWs.java
@@ -2,13 +2,15 @@ package heroes.ws.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class HeroAppWs {
+public class HeroAppWs extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HeroAppWs.class, args);
@@ -23,5 +25,9 @@ public class HeroAppWs {
             }
         };
     }
+    
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(HeroAppWs.class);
+//    }
 
 }
